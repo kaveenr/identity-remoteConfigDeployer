80c360c327f29b1644d7dcb76351a16acb0518b8
Kaveen Rodrigo <kaveenr@UKKRodrigo-WSO2s.local>
2018-06-24 12:37:38 +0530

    cloning code

--- src/main/java/GitRepositoryConnector.java
+++ src/main/java/GitRepositoryConnector.java
@@ -1,23 +1,49 @@
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
 import remotefetcher.RepositoryConnector;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Date;
 
 public class GitRepositoryConnector implements RepositoryConnector {
 
     private String uri = "";
     private String branch = "";
+    private String name = "";
+    private File repoPath;
+    private Git git;
 
-    public GitRepositoryConnector(String uri,String branch){
+    public GitRepositoryConnector(String name, String uri,String branch){
         this.branch = branch;
         this.uri = uri;
+        this.repoPath = new File("$TMPDIR/"+this.name);
     }
 
-    public void pullRepository() {
+    public void pullRepository() throws Exception {
+        this.cloneRepository(this.uri,this.branch);
+    }
+
+    private void cloneRepository(String uri, String master) throws Exception {
+        CloneCommand clone_request = Git.cloneRepository()
+                .setURI(this.uri)
+                .setDirectory(this.repoPath)
+                .setBranchesToClone(Arrays.asList(branch))
+                .setBranch(this.branch);
+        try {
+            this.git = clone_request.call();
+        } catch (Exception e){
+            throw new Exception("Clone Exception");
+        }
 
     }
 
     public InputStream getFile(File location) {
+        return null;
+    }
+
+    public Date getLastModified() {
         return null;
     }
 }
--- src/main/java/remotefetcher/RepositoryConnector.java
+++ src/main/java/remotefetcher/RepositoryConnector.java
@@ -2,8 +2,10 @@ package remotefetcher;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.Date;
 
 public interface RepositoryConnector {
-    public void pullRepository();
+    public void pullRepository() throws Exception;
     public InputStream getFile(File location);
+    public Date getLastModified();
 }
